fa95b52bc2cf88b087f11846c39066b4c8f20108
Nandini602 <107759330+Nandini602@users.noreply.github.com>
2022-06-26 12:33:30 +0530

    Add files via upload

---
+++ python lab/lab7/size of tuple.py
@@ -0,0 +1,3 @@
+#python program to find the size of tuple
+tuple1=("nandini",1,2,3,4)
+print("size of tuple is",tuple1.__sizeof__())
---
+++ python lab/lab7/sum of tuple elements.py
@@ -0,0 +1,5 @@
+#python program to find the sum ofthe tuple elements
+tuple1 = ([7,8],[9,1],[10,7])
+print("the original tuple is:"+str(sum))
+res=sum(list(map(sum,list(tuple1))))
+print("the sum of the tuple elemets is:"+str(res))
