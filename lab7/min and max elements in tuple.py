ac92a7f91f5f2fcba75a4b77e0c1760f6adf24f2
Nandini602 <107759330+Nandini602@users.noreply.github.com>
2022-06-26 12:34:02 +0530

    Delete lab7 directory

--- lab7/min and max elements in tuple.py
+++
@@ -1,7 +0,0 @@
-#python program to find the max and min element in the tuple
-tuple=( 1,2,3,4)
-a=max(tuple)
-print(a)
-
-c= min(tuple)
-print(c)
--- lab7/sort the tuple .py
+++
@@ -1,6 +0,0 @@
-#python program  to sort the tuple by total digit
-
-tuple=[(45,67),(3,4,6,723),(1234,),(134,234,34)]
-print("original list is:" + str(tuple))
-result=sorted(tuple,key=lambda tup : sum([len(str(element)) for element in tup]))
-print("sortted tuple:"+ str(result))
